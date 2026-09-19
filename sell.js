362206c6e5b201f8f289cd9b238c46565df63e1e
Jesal-ai <jesalpurohitdpsv@gmail.com>
2026-09-20 01:34:18 +0530

    Update Cloudinary upload preset in sell.js

--- sell.js
+++ sell.js
@@ -71,7 +71,7 @@ form.addEventListener('submit', async (event) => {
     for (const file of photoFiles) {
       const formData = new FormData();
       formData.append("file", file);
-      formData.append("upload_preset", "thrift_upload"); // your Cloudinary preset
+      formData.append("upload_preset", "Thrift India"); // your Cloudinary preset
 
       const res = await fetch("https://api.cloudinary.com/v1_1/s29vhzjw/image/upload", {
         method: "POST",
