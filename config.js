78359e35be48e63200909a929020462f37dd3eb0
Jesal-ai <jesalpurohitdpsv@gmail.com>
2026-09-19 20:36:46 +0530

    Add Firebase configuration to config.js

--- config.js
+++ config.js
@@ -1,2 +1,15 @@
 // Add your OpenCage API key here. In production, proxy geocoding requests through your server.
 window.OPENCAGE_API_KEY = '9581992c6a3a42b2861728748bad29a7';
+// config.js
+const firebaseConfig = {
+  apiKey: "AIzaSyDLz-W7FtAPc9JpLmhb-CwrY-yRplDUOKY",
+  authDomain: "thrift-india-82ce7.firebaseapp.com",
+  projectId: "thrift-india-82ce7",
+  storageBucket: "thrift-india-82ce7.firebasestorage.app",
+  messagingSenderId: "121493886406",
+  appId: "1:121493886406:web:fdb59fac99ba4df4324ec7"
+};
+
+firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
+const db = firebase.firestore();
